a859d975afda82ea7f0f28a91c937a7caf0c2274
Joao Carlos Abreu Lopes <lopes.joao.ca@gmail.com>
2024-10-17 23:35:37 +0100

    Added swagger doc for the project.

---
+++ docs/swagger.ts
@@ -0,0 +1,20 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import { ObjectId } from 'mongodb';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Workouts Service API',
+      version: '1.0.0',
+      description: 'API documentation for Workout Service',
+      contact: {
+        name: "João Lopes",
+        email: "lopes.joao.ca@gmail.com"
+      },
+    },
+  },
+  apis: ['./src/routes/**/*.ts'], // Path to the API routes
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
